da8593238865d328e59a9c687ab8f4a995b4804b
mparaiso <mparaiso@online.fr>
2016-11-16 07:25:34 +0100

    license added

--- db/builder.go
+++ db/builder.go
@@ -1,3 +1,17 @@
+//    Copyright (C) 2016  mparaiso <mparaiso@online.fr>
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
 package db
 
 import (
@@ -394,12 +408,7 @@ func (b *QueryBuilder) getSQLForFrom() string {
 }
 func (b *QueryBuilder) getSQLForSelect() string {
 	query := "SELECT " + strings.Join(b.convert(b.sqlParts[select_]...), ", ")
-	// if fromPart, ok := b.sqlParts[from]; ok {
-	// 	query += " FROM " + strings.Join(b.convert(fromPart...), ", ")
-	// }
-	// if joinPart, ok := b.sqlParts[join]; ok {
-	// 	query += " " + strings.Join(b.convert(joinPart...), " ")
-	// }
+
 	query += b.getSQLForFrom()
 	if wherePart, ok := b.sqlParts[where]; ok {
 		query += " WHERE " + strings.Join(b.convert(wherePart...), "")
